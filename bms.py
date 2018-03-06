0c6ca4b384f0e7945310d5340c4b02d1fb407212
Vishal Gupta <vishalg8897@gmail.com>
2018-03-06 14:42:49 +0530

    Added seat scraper for BookMyShow

--- bms.py
+++ bms.py
@@ -1,23 +1,50 @@
 from bs4 import BeautifulSoup as soup
 from urllib2 import urlopen
+import requests
+import json
+from pprint import pprint
+import re
+
+SEATPOSTURL = "https://in.bookmyshow.com/serv/doSecureTrans.bms"
+
+allowed_venues = {'Sathyam Cinemas - Royapettah, Chennai',
+                  'Mayajaal Multiplex: Chennai',
+                  'S2 Theyagaraja - Thiruvanmiyur, Chennai',
+                  'Luxe Cinemas: Chennai',
+                  'Escape - Express Avenue Mall, Royapettah, Chennai',
+                  'AGS Cinemas OMR: Navlur'}
 
 
 def parse_movie_site(url):
+    print "URL:", url
     s = soup(urlopen(url).read(), 'lxml')
     shows = {}
     for venue in s.select('#venuelist > li'):
         venue_name = venue.select_one('.__venue-name').text.strip()
+        if venue_name not in allowed_venues:
+            continue
         vn = []
         for show in venue.select('.body .__showtime-link'):
             full = show.text
+            showid = show.attrs['href'].split('/')[-1]
             try:
                 mov_class = venue.select_one(
                     '.body .__showtime-link span').text
             except:
                 continue
-            showtime = full.replace(mov_class, '').strip()
-            prices = show.attrs['data-cat-popup']
-            vn.append({'time': showtime, 'class': mov_class, 'prices': prices})
+            showtime = full.replace(mov_class, '').replace(
+                'DOLBY-ATMOS', '').strip()
+            prices = json.loads(show.attrs['data-cat-popup'])
+            try:
+                seat_status = get_seats(showid)[1]
+            except:
+                seat_status = '*****'
+                print venue_name, showtime, "Failed to get seats"
+            red_prices = []
+            for p in prices:
+                red_prices.append({'price': p['price'], 'desc': p['desc']})
+            vn.append({'time': showtime, 'class': mov_class, 'prices': red_prices,
+                       'showid': showid, 'status': seat_status})
         shows[venue_name] = vn
     return shows
 
@@ -62,6 +89,40 @@ def main():
     return movies
 
 
+def get_seats(showID):
+    payload = "a=WEB&v=ACON&t=0&c=GETSEATLAYOUT&p1=" + \
+        showID + "&p2=WEB&p3=&p4=&p5=Y&p6=&p7=&p8=&p9=&p10="
+    headers = {
+        'Referer': "https://in.bookmyshow.com/buytickets/the-shape-of-water-chennai/movie-chen-ET00063946-MT/20180307",
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Cache-Control': "no-cache",
+    }
+    response = requests.request(
+        "POST", SEATPOSTURL, data=payload, headers=headers)
+    print response.status_code
+    rows = re.findall('[|]([0-9]{1,3}:.+?)[|]',
+                      response.text.replace('|', '||'))
+    if len(rows) == 0:
+        print "No seat data found"
+        print response.text
+    lim = len(rows) / 5
+    ret_str = ''
+    for i in range(5):
+        total = 0
+        num_occ = 0
+        for r in rows[lim * i:lim * (i + 1)]:
+            occ = re.findall(
+                '(?:[:][A-Z]([0-9])[0-9]{2}[+][A-Z][0-9]{1,2})', r)
+            num_occ += occ.count('2')
+            total += len(occ)
+        if total == 0:
+            ret_str += '*'
+        else:
+            ret_str += str((num_occ * 9) / total)
+    return rows, ret_str
+
+
 if __name__ == "__main__":
-    movies = main()
-    print get_shows(movies['The Shape of Water'])
+    # movies = main()
+    print get_seats('8551')
+    # pprint(get_shows(movies['The Shape of Water']))
